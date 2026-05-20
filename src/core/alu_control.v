031e89a65b240ee745b57d7d78e095bbc81bbb94
SairamKesanapalli <sairamkesanapalli@gmail.com>
2026-05-20 15:09:17 +0530

    Updates alu_control to support mul, div operations

--- src/core/alu_control.v
+++ src/core/alu_control.v
@@ -2,7 +2,9 @@ module alu_control(
     input [2:0] ALU_OP,
     input [2:0] funct3,
     input [6:0] funct7,
-    output reg [3:0] alu_op
+    output reg [3:0] alu_op,
+    output reg is_mul_div,
+    output reg [2:0] md_op
 );
 
 localparam ADD = 4'd0;
@@ -16,6 +18,10 @@ localparam SRA = 4'd10;
 localparam SLT = 4'd11;
 
     always @(*) begin
+        // Default outputs
+        is_mul_div = 1'b0;
+        md_op      = 3'b000;
+        
         case (ALU_OP)
         3'd0: begin
             case(funct3)
@@ -30,7 +36,10 @@ localparam SLT = 4'd11;
                             alu_op = SRL;
                         else if(funct7 == 7'b0100000)
                             alu_op = SRA;
+                        else
+                            alu_op = ADD;
                     end
+                default: alu_op = ADD;
             endcase
         end
         3'd1: begin                                    // B
@@ -41,21 +50,29 @@ localparam SLT = 4'd11;
                 3'b101 : alu_op = SUB;
                 3'b110 : alu_op = SUB;
                 3'b111 : alu_op = SUB;
+                default: alu_op = SUB;
             endcase
         end
         3'd2: begin
-            case({funct7, funct3})
-                    10'b0000000000: alu_op = ADD;  // ADD
-                    10'b0100000000: alu_op = SUB;  // SUB
-                    10'b0000000111: alu_op = AND;
-                    10'b0000000110: alu_op = OR;
-                    10'b0000000100: alu_op = XOR;
-                    10'b0000000001: alu_op = SLL;
-                    10'b0000000101: alu_op = SRL;
-                    10'b0100000101: alu_op = SRA;
-                    10'b0000000010: alu_op = SLT;
-                    default : alu_op = ADD;
-            endcase
+            if (funct7 == 7'b0000001) begin
+                // M-Extension (Multiplication / Division)
+                is_mul_div = 1'b1;
+                md_op      = funct3;
+                alu_op     = ADD; // Doesn't matter
+            end else begin
+                case({funct7, funct3})
+                        10'b0000000000: alu_op = ADD;  // ADD
+                        10'b0100000000: alu_op = SUB;  // SUB
+                        10'b0000000111: alu_op = AND;
+                        10'b0000000110: alu_op = OR;
+                        10'b0000000100: alu_op = XOR;
+                        10'b0000000001: alu_op = SLL;
+                        10'b0000000101: alu_op = SRL;
+                        10'b0100000101: alu_op = SRA;
+                        10'b0000000010: alu_op = SLT;
+                        default : alu_op = ADD;
+                endcase
+            end
         end
 
         3'd3: alu_op = ADD;
